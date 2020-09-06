e9ff56ee71022afb07918b0575daf6021a09c3ba
Patrick Theriot <Theriotpatrick@gmail.com>
2020-09-06 16:57:32 -0500

    added all current projects

---
+++ CMPS161 tempiture convertinater/src/FtoC.java
@@ -0,0 +1,14 @@
+//Patrick Theriot, w0702809, CMPS 161
+import java.util.Scanner;
+public class FtoC {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double celsius;
+		double fahrenheit = input.nextDouble();
+		
+		celsius = (5.0/9.0) * (fahrenheit - 32.0);
+		
+		System.out.println(celsius);
+				
+	}
+}
