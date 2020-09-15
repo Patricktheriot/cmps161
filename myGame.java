21671b6197287731dcb02cc5469386f868dc0d47
Chris Theriot <ctheriot@pecsafety.com>
2020-09-15 15:08:10 -0500

    modified to be multi-line with single command

--- myGame.java
+++ myGame.java
@@ -3,10 +3,9 @@ public class myGame {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("You are Wolf Comrad a member of an elete team of sodliers who have spetal abilitys called The Gardians Of The Past. ");
-		System.out.println("The Gardians Of The Past were sent to the planet Vestonia in order to stop Greygnare,the leader of the Vestonians, ");
-		
-		System.out.println("from using The Eye Of The Galexy, a dimond that has the power to turn back time.");
+		System.out.println("You are Wolf Comrad a member of an elete team of sodliers who have spetal abilitys called The Gardians Of The Past.\n"
+			+ "The Gardians Of The Past were sent to the planet Vestonia in order to stop Greygnare,the leader of the Vestonians,\n");
+			+ "from using The Eye Of The Galexy, a dimond that has the power to turn back time.");
 		
 	}
 
